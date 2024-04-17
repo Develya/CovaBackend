2d6936dfeb572d95228e9ae83f2155b7036213db
Develya <github@shield.paintilya.dev>
2024-04-17 08:59:08 -0400

    Initial Commit

---
+++ src/test/java/dev/develya/cova/CovaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.develya.cova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
