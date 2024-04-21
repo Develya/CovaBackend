5c655b65739a1c1814341afdd34a1f8e128abcdf
Develya <github@shield.paintilya.dev>
2024-04-21 11:04:03 -0400

    Configured CORS to accept frontend

--- src/main/java/dev/develya/cova/CovaApplication.java
+++ src/main/java/dev/develya/cova/CovaApplication.java
@@ -2,6 +2,9 @@ package dev.develya.cova;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CovaApplication {
@@ -10,4 +13,14 @@ public class CovaApplication {
 		SpringApplication.run(CovaApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsMappingConfigurer() { // Bean to configure CORS globally
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**") // Allow frontend origins
+						.allowedOrigins("http://localhost:3000", "http://192.168.240.100:3000");
+			}
+		};
+	}
 }
