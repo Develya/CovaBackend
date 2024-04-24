a15252e9ecd401b5a28be19706e03964b13185ea
Francis Beaulieu-Ménard <2072827@colval.qc.ca>
2024-04-24 14:10:17 -0400

    was quick but I add the addJourney to the backend lets go we can now start the frontend of the userstory 4 :C

--- src/main/java/dev/develya/cova/controller/JourneyRestController.java
+++ src/main/java/dev/develya/cova/controller/JourneyRestController.java
@@ -1,13 +1,16 @@
 package dev.develya.cova.controller;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import dev.develya.cova.model.Journey;
 import dev.develya.cova.model.Trajet;
 import dev.develya.cova.repository.JourneyRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,7 +18,7 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/journeys")
 public class JourneyRestController {
-
+    private Gson gson = new Gson();
     @Autowired
     JourneyRepository journeyRepository;
     @Autowired
@@ -24,6 +27,25 @@ public class JourneyRestController {
     @GetMapping("/all")
     public List<Journey> all() {
         return journeyRepository.findAll();
+    }
+
+    @PostMapping("/addJourney")
+    public ResponseEntity<?> createJourney(@Valid @RequestBody Journey journey){
+        try{
+            journeyRepository.save(journey);
+            JsonObject responseObject = new JsonObject();
+            responseObject.addProperty("success", true);
+            responseObject.addProperty("message", "journey create successfully.");
+            return ResponseEntity.status(HttpStatus.CREATED).body(gson.toJson(responseObject)); // If success
+        }catch(DataIntegrityViolationException ex){
+            // If problem with data integrity,
+            // we assume that it's related to the email already existing, since the validation logic
+            // makes sure that everything is in order
+            JsonObject errorObject = new JsonObject();
+            errorObject.addProperty("success", false);
+            errorObject.addProperty("message", "cheh");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(gson.toJson(errorObject));
+        }
     }
 
     public void findJourneywithTrajet(Optional<Trajet> trajet){
